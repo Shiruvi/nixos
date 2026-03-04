fcde0431ae2fc7efa08dfa924d623aa3743cb2de
Shiruvi <Shiruvireu@gmail.com>
2026-03-04 20:30:06 +0300

    shellinstall alias added

--- nix/Nya/configurations/fish.nix
+++ nix/Nya/configurations/fish.nix
@@ -3,5 +3,10 @@
   ...
 }:
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      shellinstall = "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && fisher install IlanCosman/tide@v6";
+    };
+  };
 }
