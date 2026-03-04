4d3f150fb4c00451ac32265639bd4ed995ef365f
Shiruvi <Shiruvireu@gmail.com>
2026-03-04 20:07:07 +0300

    configuring fish

--- nix/MeoW/configurations/fish.nix
+++ nix/MeoW/configurations/fish.nix
@@ -14,6 +14,6 @@
           rev = "v6.0.1";  # Используй последнюю версию
           sha256 = "sha256-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX="; # Нужно будет заменить
         };
-      };
+      }
       ];
 }
