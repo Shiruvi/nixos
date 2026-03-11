830cd1788cdf146431777a637ca3ecbff9518f37
Shiruvi <Shiruvireu@gmail.com>
2026-03-11 20:36:04 +0300

    otd configs delete

--- home/Shiruvi/store/files.nix
+++ home/Shiruvi/store/files.nix
@@ -1,7 +1,6 @@
 {
   home.file = {
     ".config/nvim".source = ./nvim;
-    ".config/OpenTabletDriver".source = ./OpenTabletDriver;
     ".config/fuzzel".source = ./fuzzel;
     ".config/kitty".source = ./kitty;
     ".config/mako".source = ./mako;
--- nix/MeoW/configurations/otd.nix
+++ nix/MeoW/configurations/otd.nix
@@ -1,5 +1,6 @@
 {
   hardware.opentabletdriver.enable = true;
+  hardware.opentabletdriver.daemon.enable = true;
   hardware.uinput.enable = true;
   boot.kernelModules = [ "uinput" ];
 }
