fd1e211b85ac25be4b901cc1f446595012c91e9c
Shiruvi <Shiruvireu@gmail.com>
2026-04-09 08:58:08 +0300

    adding gamemode

--- nix/Nya/configurations/gaming.nix
+++ nix/Nya/configurations/gaming.nix
@@ -7,9 +7,16 @@
     enable = true;
     package = pkgs.power-profiles-daemon;
   };
-  programs.steam = {
-    enable = true;
+  programs = {
+    steam = {
+      enable = true;
+    };
+    gamemode = {
+      enable = true;
+      enableRenice = true;
+    };
   };
+
   environment.systemPackages = with pkgs; [
     prismlauncher
     osu-lazer-bin
