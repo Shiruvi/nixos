df6fab844a31ebaf6f01e341d9107a409612333b
Shiruvi <Shiruvireu@gmail.com>
2026-04-23 10:29:00 +0300

    remove config oonlyoffice files

--- home/Shiruvi/store/files.nix
+++ home/Shiruvi/store/files.nix
@@ -4,7 +4,6 @@
     ".config/kitty".source = ./kitty;
     ".config/niri".source = ./niri;
     ".config/fastfetch".source = ./fastfetch;
-    ".config/onlyoffice".source = ./onlyoffice;
     ".config/superfile".source = ./superfile;
     ".config/fuzzel".source = ./fuzzel;
     ".config/mako".source = ./mako;
