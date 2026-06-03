e3e20594631f15a9784a5d14ee1b6141dea1ab22
Shiruvi <Shiruvireu@gmail.com>
2026-06-04 00:40:28 +0300

    syncthing configure

--- nix/MeoW/services/sync.nix
+++ nix/MeoW/services/sync.nix
@@ -2,6 +2,8 @@
   services.syncthing = {
     enable = true;
     user = "Shiruvi";
+    dataDir = "/home/Shiruvi/Sync";
+    configDir = "/home/Shiruvi/.config/syncthing";
   };
   networking.firewall.allowedTCPPorts = [ 22000 ];
   networking.firewall.allowedUDPPorts = [
--- nix/Nya/services/sync.nix
+++ nix/Nya/services/sync.nix
@@ -1,5 +1,10 @@
 {
-  services.syncthing.enable = true;
+  services.syncthing = {
+    enable = true;
+    user = "Shiruvi";
+    dataDir = "/home/Shiruvi/Sync";
+    configDir = "/home/Shiruvi/.config/syncthing";
+  };
   networking.firewall.allowedTCPPorts = [ 22000 ];
   networking.firewall.allowedUDPPorts = [
     21027
