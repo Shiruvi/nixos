d81b79175e372a24b7b51bc1486ce1c4172211d6
Shiruvi <Shiruvireu@gmail.com>
2026-06-04 22:23:51 +0300

    jp adding

---
+++ nix/MeoW/makeconf/jp.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  ...
+}:
+{
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+      ];
+    };
+  };
+  environment.variables = {
+    XMODIFIERS = "@im=fcitx";
+  };
+}
